5d8ab7adabe4d45fd5dd214d98e91d9c259f950d
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 03:50:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -13,13 +13,11 @@ end
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
-  binding.pry
   if hash[key]
     hash[key] += 1 
   else
     hash[key] = 1 
   end
   # if the provided key is present, increment its value by 1
-  binding.pry
-  return hash[key]
+  return hash
 end
